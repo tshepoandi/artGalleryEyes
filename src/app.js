4ae31834611d6244b6bc3adf218ee5264f2f0df6
tshepomashiloane <tshepomashiloane869@gmail.com>
2023-07-27 10:46:44 +0200

    resize

--- src/app.js
+++ src/app.js
@@ -10,8 +10,15 @@
  input.mouseX.range = input.mouseX.end - input.mouseX.start;
 //  console.log(input.mouseX)
 
-let handleMouseMovement = () => {
-    console.log('event') 
+let handleMouseMovement = (event) => {
+    input.mouseX.current = event.clientX;
+    input.mouseX.fraction = (input.mouseX.current - input.mouseX.start) / input.mouseX.range 
+    console.log(input.mouseX.fraction) 
 }
 
+let handleResize = () => {
+    input.mouseX.end = window.innerWidth;
+    input.mouseX.range = input.mouseX.end - input.mouseX.start;
+}
 window.addEventListener('mousemove', handleMouseMovement)
+window.addEventListener('resize', handleResize)

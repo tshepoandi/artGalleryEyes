f69b86814cf640303e897b1bcc594d2bf72e7d9c
tshepomashiloane <tshepomashiloane869@gmail.com>
2023-07-30 19:52:12 +0200

    api

--- react_catch_up/src/Components/index.js
+++ react_catch_up/src/Components/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import { GameQuestions, QuestionDiv, QuestionTitle } from './style'
 
 const Question = () => {
+  const [word,setWord] = useState();
+
+
+  useEffect(()=>{
+    fetch('https://opentdb.com/api.php?amount=10')
+    .then((response)=>{
+      response.json()
+    }).then((data)=> {
+      setWord(data)
+      console.log(data)
+    })
+  }, [])
   return (
-    <div>Question</div>
+    <QuestionDiv>
+      <QuestionTitle className='Question'>HI </QuestionTitle>
+      <GameQuestions ClassName='QuestionFromAPI'>
+        {word}
+      </GameQuestions>
+    </QuestionDiv>
   )
 }
 
 export default Question
+
+
+// https://opentdb.com/api.php?amount=10
---
+++ react_catch_up/src/Components/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const QuestionDiv = styled.div`
+    width:100%;
+`
+
+export const QuestionTitle = styled.h1`
+`
+
+export const GameQuestions = styled.p`
+`

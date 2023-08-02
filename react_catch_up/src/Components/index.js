075c4e4265b5281c2b3ce824674c1a8e3fbfa048
tshepomashiloane <tshepomashiloane869@gmail.com>
2023-08-02 18:34:00 +0200

    rendered questions

--- react_catch_up/src/Components/index.js
+++ react_catch_up/src/Components/index.js
@@ -3,23 +3,27 @@ import { useState,useEffect } from 'react'
 import { GameQuestions, QuestionDiv, QuestionTitle } from './style'
 
 const Question = () => {
-  const [word,setWord] = useState();
+  const [questions, setQuestions] = useState([]);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=10')
-    .then((response)=>{
-      response.json()
-    }).then((data)=> {
-      setWord(data)
-      console.log(data)
-    })
-  }, [])
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data.results);
+        console.log(data.results);
+      });
+  }, []);
+  
   return (
-    <QuestionDiv>
-      <QuestionTitle className='Question'>HI </QuestionTitle>
-      <GameQuestions ClassName='QuestionFromAPI'>
-        {word}
+   <QuestionDiv>
+      <QuestionTitle className='Question'>HI</QuestionTitle>
+      <GameQuestions className='QuestionFromAPI'>
+        {questions.map((question, index) => (
+          <div key={index}>
+            <h3>Question {index + 1}</h3>
+            <p>{question.question}</p>
+          </div>
+        ))}
       </GameQuestions>
     </QuestionDiv>
   )
